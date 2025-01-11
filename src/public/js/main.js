d70ae66b8645759639f836868ca45ec06539eedc
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2025-01-11 10:17:11 +0100

    Create main.js

---
+++ src/public/js/main.js
@@ -0,0 +1,13 @@
+import './map.js';
+import './layers.js';
+
+
+// const menuLinks = document.querySelectorAll('.menu-link');
+
+// menuLinks.forEach(link => {
+//   link.addEventListener('click', function(e) {
+//     e.preventDefault(); // Prevent default link behavior
+//     const href = this.getAttribute('href');
+//     window.location.href = href; // Redirect to the linked path
+//   });
+// });
